dc948f30035f47f1a8b0801d994d5ee3fcf80ca4
nprati01 <nicole.e.prati@gmail.com>
2023-04-12 16:29:44 -0400

    complete edit finch functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 
@@ -22,3 +22,11 @@ def finches_detail(request, finch_id):
 class FinchCreate(CreateView):
   model = Finch
   fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields =['description', 'habitat', 'note']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
