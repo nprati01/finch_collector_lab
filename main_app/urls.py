cb2cd390c6aa201755e76bef6f23c7e105f0542e
nprati01 <nicole.e.prati@gmail.com>
2023-04-13 14:24:04 -0400

    Feeding Finch functionality complete and feeding form working

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('finches/create/', views.FinchCreate.as_view(), name='finches_create'),
     path('finches/<int:pk>/update/', views.FinchUpdate.as_view(), name='finches_update'),
     path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
+    path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
 ]
